849cddfdea4366fceba922c678583bc4bbb0af22
Fadi ZAATARI <fadizaatari@hotmail.com>
2025-02-18 20:23:48 +0200

    commint 3

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <div className="flex justify-center items-center bg-gradient-to-r from-blue-950 via-blue-800 to-blue-600 h-screen">
         <p className="text-white text-7xl text-center">
           welcome to <span className="font-bold text-amber-200">REACT</span>{" "}
-          with FADI
+          using TailwindCSS
         </p>
       </div>
     </>
