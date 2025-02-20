d5b160be0be1572f3dfea9c6ba77dca26bbafef1
Fadi ZAATARI <fadizaatari@hotmail.com>
2025-02-20 16:00:33 +0200

    1

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,26 @@ function App() {
         <div className="container mx-auto px-4 py-3 font-small ">
           <ul className="flex justify-center space-x-6 text-white text-2xl">
             <li>
-              <a href="#" className="hover:shadow-white hover:shadow-2xs">
+              <a
+                href="#"
+                className="font-roboto hover:shadow-white hover:shadow-2xs"
+              >
                 About
               </a>
             </li>
             <li>
-              <a href="#" className="hover:shadow-white hover:shadow-2xs">
+              <a
+                href="#"
+                className="font-roboto hover:shadow-white hover:shadow-2xs"
+              >
                 Services
               </a>
             </li>
             <li>
-              <a href="#" className="hover:shadow-white hover:shadow-2xs">
+              <a
+                href="#"
+                className="font-roboto hover:shadow-white hover:shadow-2xs"
+              >
                 Contact
               </a>
             </li>
@@ -25,7 +34,7 @@ function App() {
         </div>
       </div>
       <div className="flex justify-center items-center bg-gradient-to-r from-blue-950 via-blue-800 to-blue-600 h-screen">
-        <p className="text-white text-7xl text-center">
+        <p className="text-white text-7xl text-center font-roboto">
           welcome to <span className="font-bold text-amber-200">REACT</span>{" "}
           using TailwindCSS
         </p>
