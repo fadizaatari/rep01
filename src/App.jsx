f034f05ee2b85b004654ecb9a0948a80c6b5078d
Fadi ZAATARI <fadizaatari@hotmail.com>
2025-02-20 12:54:57 +0200

    2

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <div className="fixed top-0 left-0 w-full shadow-md z-50">
-        <div className="container mx-auto px-4 py-3 font-small">
+        <div className="container mx-auto px-4 py-3 font-small ">
           <ul className="flex justify-center space-x-6 text-white text-2xl">
             <li>
               <a href="#" className="hover:shadow-white hover:shadow-2xs">
