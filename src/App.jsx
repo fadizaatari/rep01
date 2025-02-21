5ea56e9e2cd40aea58dce649d18525b4fd6af523
Fadi ZAATARI <fadizaatari@hotmail.com>
2025-02-21 20:43:28 +0200

    3

--- src/App.jsx
+++ src/App.jsx
@@ -35,8 +35,8 @@ function App() {
       </div>
       <div className="flex justify-center items-center bg-gradient-to-r from-blue-950 via-blue-800 to-blue-600 h-screen">
         <p className="text-white text-7xl text-center font-roboto">
-          welcome to{" "}
-          <span className="font-doto font-bold text-amber-200">REACT</span>
+          welcome to
+          <span className="font-doto font-bold text-amber-200"> REACT </span>
           using
           <span className="font-orbitron"> TailwindCSS</span>
         </p>
