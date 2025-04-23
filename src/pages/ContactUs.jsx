b3a5a7c842fa171915d8758db128efa032f12292
fadizaatari <155579635+fadizaatari@users.noreply.github.com>
2025-04-23 11:18:53 +0300

    Update ContactUs.jsx

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -6,7 +6,7 @@ function ContactUs() {
 
       <div className="flex justify-center items-center bg-gradient-to-r from-blue-950 via-blue-800 to-blue-600 h-screen">
         <p className="text-white text-7xl text-center font-roboto">
-          Contact Us
+          Contact Us Right now
         </p>
       </div>
     </>
