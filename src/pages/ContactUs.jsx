0a37eb6c1be1927d8ac469f1d7c4776c26c10de4
Fadi ZAATARI <fadizaatari@hotmail.com>
2025-04-23 11:32:19 +0300

    02

--- src/pages/ContactUs.jsx
+++ src/pages/ContactUs.jsx
@@ -5,8 +5,8 @@ function ContactUs() {
       <Header />
 
       <div className="flex justify-center items-center bg-gradient-to-r from-blue-950 via-blue-800 to-blue-600 h-screen">
-        <p className="text-red-400  text-7xl text-center font-roboto">
-          Contact Us Right now for testing...
+        <p className="text-white  text-7xl text-center font-roboto">
+          Contact Us version 2
         </p>
       </div>
     </>
