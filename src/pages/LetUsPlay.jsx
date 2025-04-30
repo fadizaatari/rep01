69a59454c0e1a51c7f382c4d0f076097c8050941
fadizaatari <155579635+fadizaatari@users.noreply.github.com>
2025-04-30 08:28:29 +0300

    Update LetUsPlay.jsx

--- src/pages/LetUsPlay.jsx
+++ src/pages/LetUsPlay.jsx
@@ -3,7 +3,7 @@ import Header from "../components/Header";
 function LetUsPlay() {
   return (
     <>
-      <Header />
+
 
       <div className="flex justify-center items-center bg-gradient-to-r from-blue-950 via-blue-800 to-blue-600 h-screen">
         <p className="text-white text-7xl text-center font-roboto">
