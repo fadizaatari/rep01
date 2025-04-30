7537f38897974d77aa457d664246a85b2c02bdb6
fadizaatari <155579635+fadizaatari@users.noreply.github.com>
2025-04-30 08:10:45 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Header from "./components/Header";
 function App() {
   return (
     <>
-      <Header />
+        <h1>I| am testing xxx</h1>
 
       <div className="flex justify-center items-center bg-gradient-to-r from-blue-950 via-blue-800 to-blue-600 h-screen">
         <p className="text-white text-7xl text-center font-roboto">
