d1d6cc2974611b1ed5f30c28263d2be2b49687b8
Fadi ZAATARI <fadizaatari@hotmail.com>
2025-04-30 16:49:43 +0300

    2

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
   return (
     <>
       <div className="fixed top-0 left-0 w-full shadow-md z-50 bg-blue-950">
-        <div className="container mx-auto px-4 py-3 font-small ">
+        <div className="container mx-auto px-4 py-2 font-small ">
           <ul className="flex justify-center space-x-6 text-white text-2xl">
             <List title="About" link="/" />
             <List title="Tic Tac Toe" link="/tiktaktoe" />
